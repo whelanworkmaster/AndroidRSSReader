4e78cd134bf9fae58e9e54440eb8c6460a7b2534
whelanworkmaster <christianworkmaster@gmail.com>
2017-03-13 14:54:38 -0400

    Build 3
    Built an apk and tested on Samsung Galaxy S7

--- RssApp/app/src/main/java/com/example/whelan/rssapp/MainActivity.java
+++ RssApp/app/src/main/java/com/example/whelan/rssapp/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.whelan.rssapp;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.Xml;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -23,6 +28,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private SwipeRefreshLayout mSwipeLayout;
     private Button mBtnFetch;
     private EditText mEditTextFetch;
     private TextView mTextViewTitle;
@@ -41,15 +48,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBtnFetch = (Button) findViewById(R.id.btnFetch);
-        mEditTextFetch = (EditText) findViewById(R.id.editTextFetch);
-        mTextViewTitle = (TextView) findViewById(R.id.textViewTitle);
-        mTextViewDesc = (TextView) findViewById(R.id.textViewDesc);
-        mTextViewLink = (TextView) findViewById(R.id.textViewLink);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mSwipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        mBtnFetch = (Button) findViewById(R.id.fetchFeedButton);
+        mEditTextFetch = (EditText) findViewById(R.id.rssFeedEditText);
+        mTextViewTitle = (TextView) findViewById(R.id.feedTitle);
+        mTextViewDesc = (TextView) findViewById(R.id.feedDescription);
+        mTextViewLink = (TextView) findViewById(R.id.feedLink);
 
-        mBtnFetch.setOnClickListener(new View.OnClickListener()  {
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mBtnFetch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                new FetchFeedTask().execute((Void) null);
+            }
+        });
+
+        mSwipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
                 new FetchFeedTask().execute((Void) null);
             }
         });
@@ -61,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPreExecute() {
+            mSwipeLayout.setRefreshing(true);
             urlLink = mEditTextFetch.getText().toString();
         }
 
@@ -71,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             try {
-                // To check if the input has the correct url formattingd
+                // To check if the input has the correct url formatting
                 if(!urlLink.startsWith("http://") && !urlLink.startsWith("https://")) {
                     urlLink = "http://" + urlLink;
                 }
@@ -91,14 +110,17 @@ public class MainActivity extends AppCompatActivity {
         // Only update UI in pre and post update
         @Override
         protected void onPostExecute(Boolean success) {
+            mSwipeLayout.setRefreshing(false);
 
             if(success) {
                 mTextViewTitle.setText("Feed Title: " + mFeedTitle);
                 mTextViewDesc.setText("Feed Description: " + mFeedDesc);
                 mTextViewLink.setText("Feed Link: " + mFeedLink);
+                mRecyclerView.setAdapter(new RssFeedListAdapter(mFeedModelList));
             } else {
                 Toast.makeText(MainActivity.this,
-                        "Enter a valid Rss feed URL", Toast.LENGTH_LONG).show();
+                        "Enter a valid Rss feed URL",
+                        Toast.LENGTH_LONG).show();
             }
         }
     }
@@ -190,6 +212,45 @@ public class MainActivity extends AppCompatActivity {
             this.title = title;
             this.link = link;
             this.desc = desc;
+        }
+    }
+
+    public class RssFeedListAdapter
+            extends RecyclerView.Adapter<RssFeedListAdapter.FeedModelViewHolder> {
+
+        private List<RssFeedModel> mRssFeedModels;
+
+        public class FeedModelViewHolder extends RecyclerView.ViewHolder {
+            private View rssFeedView;
+
+            public FeedModelViewHolder(View v) {
+                super(v);
+                rssFeedView = v;
+            }
+        }
+
+        public RssFeedListAdapter(List<RssFeedModel> rssFeedModels) {
+            mRssFeedModels = rssFeedModels;
+        }
+
+        @Override
+        public FeedModelViewHolder onCreateViewHolder(ViewGroup parent, int type) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_rss_feed, parent, false);
+            FeedModelViewHolder holder = new FeedModelViewHolder(v);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(FeedModelViewHolder holder, int position) {
+            final RssFeedModel rssFeedModel = mRssFeedModels.get(position);
+            ((TextView)holder.rssFeedView.findViewById(R.id.titleText)).setText(rssFeedModel.title);
+            ((TextView)holder.rssFeedView.findViewById(R.id.descriptionText)).setText(rssFeedModel.desc);
+            ((TextView)holder.rssFeedView.findViewById(R.id.linkText)).setText(rssFeedModel.link);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mRssFeedModels.size();
         }
     }
 }
